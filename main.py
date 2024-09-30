3084fb48db830744983a7cfce3eeb2cf5f409788
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-09-30 15:11:01 -0500

    Hosting Demo Files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:black'>STARSHIP JOUSTING</h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
