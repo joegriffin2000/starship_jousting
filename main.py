79222367e4a356504b035939b847d928bc8896cd
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-10-18 12:44:52 -0500

    Reverting Workaround Attempts
    Flask-Talisman didn't work so I'm reverting changes.

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from flask_talisman import Talisman
 
 
 app = Flask(__name__)
-talisman = Talisman(app) 
+# talisman = Talisman(app) 
 
-csp = {
-    'default-src': [
-        '\'self\'',
-        'https://code.jquery.com',
-        'https://cdn.jsdelivr.net'
-    ]
-}
-# HTTP Strict Transport Security (HSTS) Header
-hsts = {
-    'max-age': 31536000,
-    'includeSubDomains': True
-}
-# Enforce HTTPS and other headers
-talisman.force_https = True
-talisman.force_file_save = True
-talisman.x_xss_protection = True
-talisman.session_cookie_secure = True
-talisman.session_cookie_samesite = 'Lax'
-talisman.frame_options_allow_from = 'https://www.google.com'
+# csp = {
+#     'default-src': [
+#         '\'self\'',
+#         'https://code.jquery.com',
+#         'https://cdn.jsdelivr.net'
+#     ]
+# }
+# # HTTP Strict Transport Security (HSTS) Header
+# hsts = {
+#     'max-age': 31536000,
+#     'includeSubDomains': True
+# }
+# # Enforce HTTPS and other headers
+# talisman.force_https = True
+# talisman.force_file_save = True
+# talisman.x_xss_protection = True
+# talisman.session_cookie_secure = True
+# talisman.session_cookie_samesite = 'Lax'
+# talisman.frame_options_allow_from = 'https://www.google.com'
  
-# Add the headers to Talisman
-talisman.content_security_policy = csp
-talisman.strict_transport_security = hsts
+# # Add the headers to Talisman
+# talisman.content_security_policy = csp
+# talisman.strict_transport_security = hsts
 
 @app.route("/")
 def hello():
