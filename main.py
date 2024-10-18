4b2354dc7b970d643fde1b04170358752a7efc11
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-10-18 12:15:53 -0500

    using Flask-Talisman to enforce HTTPS
    this should secure the flask application but irdk

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_talisman import Talisman
+
+
 app = Flask(__name__)
+talisman = Talisman(app) 
+
+talisman.force_https = True
+talisman.force_file_save = True
+talisman.x_xss_protection = True
+talisman.session_cookie_secure = True
+talisman.session_cookie_samesite = 'Lax'
+talisman.frame_options_allow_from = 'https://www.google.com'
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',ssl_context=('../cert.pem', '../key.pem'))
+    app.run(host='0.0.0.0')
