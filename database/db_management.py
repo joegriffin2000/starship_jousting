a8c65cce34a71d8b00aa2b83db30a2a7612399b7
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-11-25 15:57:10 -0600

    implementing database connection

---
+++ database/db_management.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, url_for
+from database.db_config import DB_HOST,DB_NAME,DB_USER,DB_PASS
+import pymysql.cursors
+
+def create_connection():
+        connection = pymysql.connect(host=DB_HOST,
+                                        user=DB_USER,
+                                        password=DB_PASS,
+                                        database=DB_NAME,
+                                        charset='utf8mb4',
+                                        cursorclass=pymysql.cursors.DictCursor)
+        return connection
+
+def pullTop20FromDatabase():
+    con = create_connection()
+    res = ""
+    with con:
+        with con.cursor() as cursor:
+            cursor.execute("SELECT * FROM leaderboard LIMIT 20")
+            res = cursor.fetchall()
+
+    return res
+
+def push1ToDatabase(user:str,score:int) -> str:
+    con = create_connection()
+    with con:
+        with con.cursor() as cursor:
+            sql = "INSERT INTO `leaderboard` (`user`, `score`) VALUES (%s, %s)"
+            cursor.execute(sql,(user,str(score)))
+
+        con.commit()
+
+    return "Insertion Successful"
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for
+from database.db_management import pullTop20FromDatabase,push1ToDatabase
 
 app = Flask(__name__)
+
+@app.route("/leaderboard_push1",methods=['POST'])
+def leaderboard_push1():
+    push1ToDatabase("player1",100)
+    return "<h1>CONNECTION SUCCESSFUL</h1>"
+
+@app.route("/leaderboard_pull20",methods=['GET'])
+def leaderboard_pull20():
+    return pullTop20FromDatabase()
 
 @app.route("/")
 def hello():
